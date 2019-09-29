36de05c12cdcac679f8ddb375e88959a23108a15
piglovesyou <thepiglovesyou@gmail.com>
2019-09-29 12:33:59 +0900

    implement "from"
    * Travis

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,31 @@
-import { yeah } from './index'
+import { promisify } from 'util';
+import { pipeline as _pipeline } from 'readable-stream';
+// Use Node native "stream" for per-versions compatible testing
+import { Transform } from 'stream';
+import assert from 'assert';
+import { from } from './index';
 
-test('should return false given external link', () => {
-  yeah();
+const pipeline = promisify(_pipeline);
+
+describe('from', () => {
+  test('should create a readable stream from an array', async () => {
+    const source = [ 2, 3, 4 ];
+    const expect = [ 20, 30, 40 ];
+    const actual: number[] = [];
+
+    await pipeline(
+        from(source),
+        new Transform({
+          writableObjectMode: true,
+          transform(chunk, encoding: string, callback: (error?: (Error | null), data?: any) => void): void {
+            setTimeout(() => {
+              actual.push(chunk * 10);
+              callback();
+            }, Math.random() * 100);
+          }
+        }),
+    );
+
+    assert.deepStrictEqual(actual, expect);
+  }, 10 * 1000);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
-export function yeah() {
-  console.log('yeah');
+import { Readable, ReadableOptions } from "readable-stream";
+
+export function from(iterable: IterableIterator<any> | Array<any>, opts?: ReadableOptions) {
+  if (Array.isArray(iterable)) {
+    iterable = iterable.entries();
+  }
+
+  return new Readable({
+    ...opts,
+    objectMode: true,
+    read(size: number): void {
+      for (let [k, value] of iterable) {
+        const canPushMore = this.push(value);
+        if (canPushMore === false) return;
+      }
+      this.push(null);
+    },
+  });
 }
+
